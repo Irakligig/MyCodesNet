02cf16c1c076de9f2d7c62fe84b810a249da12bd
Irakligig <darkbrineyt@gmail.com>
2025-06-20 15:40:12 +0400

    added first file

---
+++ Synchronisation1/Synchronisation1/Cache.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Synchronisation1
+{
+    
+    internal class Cache
+    {
+        public static ReaderWriterLockSlim CacheLock = new ReaderWriterLockSlim();
+        public static Dictionary<string,int> container = new Dictionary<string, int>();
+        public int? Get(string key)
+        {
+            CacheLock.EnterReadLock();
+            bool exists = container.TryGetValue(key,out int value);
+            if (exists)
+            {
+                CacheLock.ExitReadLock();
+                return value;
+            }
+            else
+            {
+                CacheLock.ExitReadLock();
+                return null;
+            }
+        }
+        public void AddOrUpdate(string key , int value)
+        {
+
+        }
+
+        public int GetOrAdd(string key , Func<int> valueFactory)
+        {
+
+        }
+    }
+}
